2b5724f981c7e49113dae5e9ee0e87de1a1cb462
Vignesh D <vignesh.d9123@gmail.com>
2025-04-02 23:53:26 +0530

    change

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,11 +1,7 @@
 'use server'
 export const trendsFromReddit = async () => {
     try {
-      const response = await fetch(`https://www.reddit.com/top.json?limit=5`, {
-        headers: {
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
-        }
-      })
+      const response = await fetch(`https://www.reddit.com/top.json?limit=5`)
       if (!response.ok) {
         console.log('Reddit:response',response)
         throw new Error('Failed to fetch trends')
